e774b44d3ae5ed1c667865dbf9b085a9058e40b3
morganmox <morgan_mox@hotmail.com>
2019-06-20 10:25:47 +0700

    register

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,15 +1,21 @@
 <template>
-    <div class="q-gutter-md" style="max-width: 300px">
-      <q-input v-model="text" label="Username" />
-      <q-input v-model="pwd" label="Password" />
-      <q-btn color="primary" label="Click" />
+  <!-- Default form login -->
+  <form>
+    <p class="h4 text-center mb-4">Sign in</p>
+    <label for="defaultFormLoginEmailEx" class="grey-text">Your email </label>
+    <input type="email" id="defaultFormLoginEmailEx" class="form-control"/>
+    <br/>
+    <label for="defaultFormLoginPasswordEx" class="grey-text">Your password </label>
+    <input type="password" id="defaultFormLoginPasswordEx" class="form-control"/>
+    <div class="text-center mt-4">
+      <button class="btn btn-indigo" type="submit">Login</button>
     </div>
+  </form>
+  <!-- Default form login -->
 </template>
+
 <style>
 </style>
 
 <script>
-export default {
-  name: 'PageIndex'
-}
 </script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -13,6 +13,13 @@ const routes = [
     children: [
       { path: '', component: () => import('pages/Login.vue') }
     ]
+  },
+  {
+    path: '/register',
+    component: () => import('layouts/MyLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Register.vue') }
+    ]
   }
 ]
 
