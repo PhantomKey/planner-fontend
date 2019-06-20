3b4f48d86251a8f85e99e9636cb6a4bb44945bfa
juinut <juinut@gmail.com>
2019-06-20 16:26:09 +0700

    test google map api

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="q-app">
     <router-view />
+    <google-map />
   </div>
 </template>
 
 <script>
+import GoogleMap from './components/GoogleMap.vue'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    GoogleMap
+  }
 }
 </script>
 
