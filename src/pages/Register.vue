b7e43f44d9500a361417ad625ddd7ad0cbf2d32f
PhantomKey <punstack@hotmail.com>
2019-06-24 10:57:16 +0700

    change register format

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -3,18 +3,26 @@
   <form class="">
     <br>
     <p>Sign up</p>
-    <p for="defaultFormRegisterNameEx" class="grey-text">Your name</p>
-    <input type="text" id="defaultFormRegisterNameEx"/>
+    <p class="grey-text">Your username</p>
+    <input type="text" id="username"/>
+    <br>
+    <p class="grey-text">Your Firstname</p>
+    <input type="text" id="firstname"/>
     <br/>
-    <p for="defaultFormRegisterEmailEx" class="grey-text">Your email</p>
-    <input type="email" id="defaultFormRegisterEmailEx"/>
-    <br/>
-    <p for="defaultFormRegisterConfirmEx" class="grey-text">Confirm your email</p>
-    <input type="email" id="defaultFormRegisterConfirmEx"/>
+    <p class="grey-text">Your Lastname</p>
+    <input type="text" id="lastname"/>
+    <br>
+    <p class="grey-text">Your email</p>
+    <input type="email" id="Email"/>
     <br/>
     <p for="defaultFormRegisterPasswordEx" class="grey-text">Your password</p>
     <input type="password" id="defaultFormRegisterPasswordEx"/>
     <br>
+    <p class="grey-text">Confirm your password</p>
+    <input type="confirm_password" id="confirm_password"/>
+    <br>
+    <p class="gray-text">Your birthday</p>
+    <input type="date" id="BoD" name="birthday">
     <p></p>
     <div class="text-center mt-4">
       <button class="btn btn-unique" type="submit">Register</button>
