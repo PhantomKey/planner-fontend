7d9aa0c1cda03b400bd38c6f1cc39cf01fbf3c3b
morganmox <morgan_mox@hotmail.com>
2019-06-25 16:25:15 +0700

    edit index

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -14,20 +14,17 @@ div{
     <register-button></register-button>
     <br>
     <p></p>
-    <add-friend></add-friend>
   </div>
 </template>
 
 <script>
 import LoginButton from '../components/LoginButton.vue'
 import RegisterButton from '../components/RegisterButton.vue'
-import AddFriend from '../components/AddFriend.vue'
 export default {
   name: 'PageIndex',
   components: {
     'login-button': LoginButton,
     'register-button': RegisterButton,
-    'add-friend': AddFriend
   }
 
 }
