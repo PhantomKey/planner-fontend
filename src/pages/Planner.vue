e862c5140c9494fd7da65910c9d527380fc1d880
Supawit Y.Pradorn <supawit.win@Supawits-MacBook-Air.local>
2019-06-28 15:45:11 +0700

    edit function to work with backend

--- src/pages/Planner.vue
+++ src/pages/Planner.vue
@@ -27,32 +27,28 @@ export default {
     authenfunction() {
       this.isLogin()
       this.isYourPlanner()
-      console.log('hahah')
       return this.isLogin() || this.isYourPlanner()
     },
     isLogin() {
-      console.log(localStorage.token)
       if (localStorage.token) {
-        console.log('woohoo')
         return true
       } else {
-        console.log('nooo')
         return false
       }
     },
     isYourPlanner() {
-      var planner_id = this.getUrlVars()["planner_id"]
-      this.$http.post ('/planner/view_planner/'+planner_id, {jwttoken: localStorage.token})
-      .then(value => console.log(value))
+      var planner_id = this.getPlannerIDfromURL()
+      console.log(planner_id)
+      this.$http.get ('/planner/view_planner/planner_id='+planner_id).then(value => console.log(value))
       // var returnactivitylist = this.$http.get('/planner/planner_id='+planner_id+
       // '/view_all_activity', {jwttoken: localStorage.token})
     },
-    getUrlVars() {
-      var vars = {};
-      var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-          vars[key] = value;
-        });
-        return vars;
+    getPlannerIDfromURL() {
+      const urlParams = new URLSearchParams(window.location.search);
+      console.log(window.location.search)
+      const myParam = urlParams.get("planner_id");
+      console.log(myParam)
+      return myParam
     }
   }
 }
