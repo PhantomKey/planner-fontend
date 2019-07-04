95ecb13b72a571bba1d4defd373f7a0bd15e024e
Supawit Y.Pradorn <supawit.win@Supawits-MacBook-Air.local>
2019-07-04 14:45:40 +0700

    edit some UI

--- src/components/CreateActivity.vue
+++ src/components/CreateActivity.vue
@@ -5,42 +5,32 @@
         @click="icon = true">
         </q-btn>
         <q-dialog v-model="icon">
-            <q-card dense style="min-width:600px;max-width:600px">
+            <q-card dense style="min-width:50%;max-width:50%">
             <q-card-section class="row items-center">
                 <div class="text-h6" style="margin:0 auto;">Create New Activity</div>
             </q-card-section>
             <q-card-section class="q-gutter-sm">
-                <q-input ref="input" filled v-model="name" label="Name"
-                ></q-input>
-                <q-select filled v-model="stype" :options="type" label="Type" >
-                </q-select>
-                <div class="row q-col-gutter-sm q-gutter-sm">
-                  <div style="width: 400px">
-                    <q-input filled v-model="startdate" type="date" label="Start Date" stack-label>
-                      <template v-slot:prepend>
-                        <q-icon name="event" />
-                      </template>
-                    </q-input>
-                  </div>
-                  <div style="width: 168px">
-                    <q-input filled v-model="starttime" type="time" label="Start Time" stack-label>
-                    </q-input>
-                  </div>
-                </div>
-                <div class="row q-col-gutter-sm q-gutter-sm">
-                  <div style="width: 400px">
-                    <q-input filled v-model="enddate" type="date" label="End Date" stack-label>
-                      <template v-slot:prepend>
-                        <q-icon name="event" />
-                      </template>
-                    </q-input>
-                  </div>
-                  <div style="width: 168px">
-                    <q-input filled v-model="endtime" type="time" label="End Time" stack-label>
-                    </q-input>
-                  </div>
-                </div>
-                <q-input ref="input" filled v-model="description" label="Description"></q-input>
+                <q-input ref="input" filled v-model="name" label="Name" style="min-width:100%;max-width:100%"></q-input>
+                <q-select filled v-model="stype" :options="type" label="Type" style="min-width:100%;max-width:100%"></q-select>
+                <!-- <div class="row q-col-gutter-sm q-gutter-sm" style="min-width:100%;max-width:100%"> -->
+                  <q-input filled v-model="startdate" type="date" label="Start Date" stack-label style="width:100%">
+                    <template v-slot:prepend>
+                      <q-icon name="event" />
+                    </template>
+                  </q-input>
+                  <q-input filled v-model="starttime" type="time" label="Start Time" stack-label style="width:100%">
+                  </q-input>
+                <!-- </div> -->
+                <!-- <div class="row q-col-gutter-sm q-gutter-sm"> -->
+                  <q-input filled v-model="enddate" type="date" label="End Date" stack-label style="width:100%">
+                    <template v-slot:prepend>
+                      <q-icon name="event" />
+                    </template>
+                  </q-input>
+                  <q-input filled v-model="endtime" type="time" label="End Time" stack-label style="width:100%">
+                  </q-input>
+                <!-- </div> -->
+                <q-input ref="input" filled v-model="description" label="Description" style="width:100%"></q-input>
                 <google-map />
             </q-card-section>
             <q-card-section position="bottom-right" style="text-align:right">
