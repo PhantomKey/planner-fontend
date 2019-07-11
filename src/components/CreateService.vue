2041dd749371fd0d4185ed69486088de4b85a34e
PhantomKey <punstack@hotmail.com>
2019-07-11 15:16:42 +0700

    create service

--- src/components/CreateService.vue
+++ src/components/CreateService.vue
@@ -6,11 +6,11 @@
     @click="serviceW = true"/>
 
     <q-dialog v-model="serviceW"> 
-        <q-card flat  style="width:600px;max-width:600px;">
-            <q-card-section>
-                <div class="text-h5">ADD YOUR SERVICE</div>
+        <q-card flat style="width:600px;max-width:600px;">
+            <q-card-section class="row items-center" style="padding-top:32px;padding-bottom:32px">
+                <div class="text-h5" style="margin:0 auto">ADD YOUR SERVICE</div>
             </q-card-section>
-            <q-card-section>
+            <q-card-section class="q-gutter-sm">
                 <q-input filled  dense label="service name" v-model="name"/>
                  <q-list>
                     <q-item multiline tag="label" v-ripple>
@@ -29,15 +29,33 @@
                         </q-item-section>
                     </q-item>
                  </q-list>
-                 <q-slide-transition>
-                    <div v-show="!visible">
+            </q-card-section>
+            <q-card-section style="padding-left:16px;padding-bottom:0px">
+                <div class='text-h6'>Service Price</div>
+            </q-card-section>
+                <q-slide-transition style="width:100%">
+                    <div v-show="!calType" style="width:100%">
+                        <q-card-section dense class="q-gutter-sm">
+                            <q-input dense filled label="Kid Price" v-model="siglePrice.kid"
+                            />
+                            <q-input dense filled label="Adult Price" v-model="siglePrice.adult"/>
+                            <q-input dense filled label="Elderly Price" v-model="siglePrice.elderly" /> 
+                        </q-card-section>
                     </div>
                 </q-slide-transition>
-            </q-card-section>
+                <q-slide-transition>
+                    <div v-show="calType" style="width:100%">
+                        <q-card-section dense class="q-gutter-sm">
+                            <q-input dense filled label="Totle Price" v-model="groupPrice"/>
+                        </q-card-section>
+                    </div>
+                </q-slide-transition>
+                
+            
             <q-card-section position="bottom-right" style="text-align:right">
                 <div class="q-gutter-sm">
-                    <q-btn outline type="submit" color="primary" label="Create" style="text-align:right" @click="createActivityBackEnd()" v-close-popup></q-btn>
-                    <q-btn v-close-popup type="reset" label="Cancel" outline color="negative" style="text-align:right" @click="resetData()"></q-btn>
+                    <q-btn outline type="submit" color="primary" label="Create" style="text-align:right" v-close-popup></q-btn>
+                    <q-btn v-close-popup type="reset" label="Cancel" outline color="negative" style="text-align:right" ></q-btn>
                 </div>
             </q-card-section>
         </q-card>
@@ -53,9 +71,14 @@ export default{
         name:'',
         PriceType:'Single',
         calType: false,
-        type: ['person','group'],
         calname:'person',
-        calcaption:'Calculation price as a person spilt by age'
+        calcaption:'Calculation price as a person spilt by age',
+        siglePrice:{
+            kid:'',
+            adult:'',
+            elderly:''
+        },
+        groupPrice:'',
     }
   },
   methods:{
