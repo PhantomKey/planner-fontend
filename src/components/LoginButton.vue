368480ad73414a1174f4c91c72f48a86df9b5343
Supawit Y.Pradorn <supawit.win@Supawits-MacBook-Air.local>
2019-07-15 12:27:32 +0700

    more UI

--- src/components/LoginButton.vue
+++ src/components/LoginButton.vue
@@ -7,14 +7,25 @@
           @click="openLoginDialog = true">
     </q-btn>
     <q-dialog v-model="openLoginDialog">
-      <q-card dense style="min-width:40%;max-width:40%">
+      <q-card class="dialog" dense style="min-width:40%;max-width:40%;min-height:70%;,max-height:70%;">
         <q-card-section class="row justify-center">
-          <q-input filled v-model="username" label="Username" style="min-width:90%;max-width:90%"
+          <h4>Log in to Trip Planner</h4>
+          <img src="~assets/userprofileicon.png" style="max-width:30%;">
+          <q-input bg-color="#ffeeef" filled v-model="username" label="Username" style="min-width:90%;max-width:90%;"
           lazy-rules
-          :rules="[ val => val && val.length > 0 || 'Username is required']"></q-input>
-          <q-input filled type="password" v-model="password" label="Password" style="min-width:90%;max-width:90%"
+          :rules="[ val => val && val.length > 0 || '']">
+            <template v-slot:prepend>
+              <q-icon name="account_box" />
+            </template>
+          </q-input>
+          <q-input bg-color="#ffeeef" filled type="password" v-model="password" label="Password" style="min-width:90%;max-width:90%"
           lazy-rules
-          :rules="[ val => val && val.length > 0 || 'Password is required']"></q-input>
+          :rules="[ val => val && val.length > 0 || '']">
+            <template v-slot:prepend>
+              <q-icon name="lock" />
+            </template>
+          </q-input>
+          <q-btn label="Login" style="min-width:90%;max-width:90%;background:#fa928f;color:white"></q-btn>
         </q-card-section>
       </q-card>
     </q-dialog>
@@ -25,11 +36,16 @@
 export default{
   data() {
     return {
-      openLoginDialog: false
+      openLoginDialog: false,
+      username: '',
+      password: ''
     }
   }
 }
 </script>
 
 <style>
+  .dialog {
+    background-color: #fff9f9;
+  }
 </style>
