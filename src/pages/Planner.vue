a5ce4bbeda286d56c14b20da12e9238ecebb3dea
Supawit Y.Pradorn <supawit.win@Supawits-MacBook-Air.local>
2019-07-18 18:17:38 +0700

    activity allow startdate!=enddate

--- src/pages/Planner.vue
+++ src/pages/Planner.vue
@@ -1,16 +1,14 @@
 <template>
 <div>
   <div style="height:80vh">
-    <div v-if="loadingfinish" style="height:100%;margin-left:33.59px">
+    <div v-if="loadingfinish" style="height:100%;">
       <div v-if="checkplanner&&isLogin" style="height:100%">
-        <div class="row" style="height:100%">
+        <div class="row" style="height:100%;margin-left:33.59px;margin-right:33.59px">
           <div class="col-sm-4" style="background-color: white;;border-right:5px solid #ddd">
             <schedule :scheduleData="refreshToken"></schedule>
           </div>
-          <div class="col-sm-8" style="background-color: white">
- <!--           <createactivity @refreshSchedule="refreshPage"></createactivity>
-            <create-service></create-service> -->
-            <TravelMap class="travel-map"/>
+          <div class="col-sm-8" style="background-color: white;">
+            <TravelMap class="travel-map" style="height:100%"/>
           </div>
         </div>
         <createactivity @refreshSchedule="refreshPage"></createactivity>
