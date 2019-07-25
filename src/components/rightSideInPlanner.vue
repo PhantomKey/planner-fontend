016aea094ea31a39e736b7166b2507ef94171749
PhantomKey <punstack@hotmail.com>
2019-07-25 16:13:16 +0700

    connect to back-end

--- src/components/rightSideInPlanner.vue
+++ src/components/rightSideInPlanner.vue
@@ -25,9 +25,8 @@
       height="100%"
     >
       <q-carousel-slide name="details" class="column no-wrap flex-center">
-        <q-icon name="style" color="primary" size="56px" />
-        <div class="q-mt-md text-center">
-          {{ lorem }}
+        <div class="q-mt-md" style="height:100%;width:100%">
+          <result style="height:100%"/>
         </div>
       </q-carousel-slide>
       <q-carousel-slide name="route" class="no-wrap flex-center">
@@ -44,9 +43,11 @@
 </template>
 <script>
 import Directions from "../components/Directions.vue"
+import Result from '../components/Result.vue'
 export default {
   components: {
-    Directions
+    Directions,
+    Result
   },
   props:['uniqDate'],
   watch: {
